cb33c5ac0a16e9750936158fe0369011381106a4
ShiftyShadows <purple.quasar@proton.me>
2024-12-04 02:00:39 +0300

    Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+   This module defines the function: island_perimeter(grid).
+"""
+
+
+def island_perimeter(grid):
+    """
+       Calculate the perimeter of an island in a grid.
+
+       The grid is represented as a list of lists where:
+           - 0 represents water
+           - 1 represents land
+
+       Args:
+           grid (list of list of int): The grid representation
+           of the island.
+
+       Returns:
+           int: The perimeter of the island.
+    """
